3b4efaf77d49769fb842333e95f92aa02f0c9f35
MariamIssah <156105793+MariamIssah@users.noreply.github.com>
2025-05-23 21:29:57 +0200

    Update 3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,2 +1,24 @@
+#!/usr/bin/env python3
+"""
+Calculates the transpose of a matrix.
+"""
+
+
 def matrix_transpose(matrix):
-    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+    """
+    Transposes a matrix.
+
+    """
+    # Get the number of rows and cols
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    # Create a new matri to create a new transpose
+    transposed = []
+
+    for col in range(cols):
+        new_row = []
+        for row in range(rows):
+            new_row.append(matrix[row][col])
+        transposed.append(new_row)
+    return transposed
