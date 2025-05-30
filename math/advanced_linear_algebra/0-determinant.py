a4afb24764f8928744e4481e9bf66bb8e01b5322
MariamIssah <m.issah1@alustudent.com>
2025-05-30 20:39:04 +0200

    readme.md file added

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -8,7 +8,9 @@ def determinant(matrix):
     """
     Calculates the determinant of a square matrix.
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list) or not all(
+    isinstance(row, list) for row in matrix
+):
         raise TypeError("matrix must be a list of lists")
 
     if matrix == [[]]:
