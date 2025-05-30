f9ae53fb0d6741aff40685394c7a819e68886579
MariamIssah <156105793+MariamIssah@users.noreply.github.com>
2025-05-30 20:49:16 +0200

    Update 2-cofactor.py

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -1,21 +1,51 @@
 #!/usr/bin/env python3
-"""
-Calculates the cofactor matrix
-"""
-from 1-minor import minor
+"""Function  that calculates the cofactor matrix of a matrix"""
+
+
+def determinant(matrix):
+    """Function that calculates the determinant of a matrix"""
+    if len(matrix) == 2:
+        return ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]))
+    det = []
+    for i in range(len(matrix)):
+        mini = [[j for j in matrix[i]] for i in range(1, len(matrix))]
+        for j in range(len(mini)):
+            mini[j].pop(i)
+        if i % 2 == 0:
+            det.append(matrix[0][i] * determinant(mini))
+        if i % 2 == 1:
+            det.append(-1 * matrix[0][i] * determinant(mini))
+    return sum(det)
 
 
 def cofactor(matrix):
-    """
-    Calculates the cofactor matrix of a square matrix.
-    """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    """Function  that calculates the cofactor matrix of a matrix"""
+    if type(matrix) is not list or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
-    if matrix == [] or any(len(row) != len(matrix) for row in matrix):
-        raise ValueError("matrix must be a non-empty square matrix")
-
-    minors = minor(matrix)
-    for i in range(len(minors)):
-        for j in range(len(minors)):
-            minors[i][j] *= (-1) ** (i + j)
-    return minors
+    for i in matrix:
+        if type(i) is not list:
+            raise TypeError("matrix must be a list of lists")
+    for i in matrix:
+        if len(matrix) != len(i):
+            raise ValueError("matrix must be a non-empty square matrix")
+    if len(matrix) == 1 and len(matrix) == 1:
+        return [[1]]
+    if len(matrix) == 2:
+        cofactor = [i[::-1] for i in matrix]
+        cofactor = cofactor[::-1]
+        cofactor = [[cofactor[i][j] if (i + j) % 2 == 0 else -cofactor[i][j]
+                     for j in range(len(cofactor[i]))]
+                    for i in range(len(cofactor))]
+        return cofactor
+    cofactor = [[j for j in matrix[i]] for i in range(len(matrix))]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            mini = [[j for j in matrix[i]] for i in range(len(matrix))]
+            mini = mini[:i] + mini[i + 1:]
+            for k in range(len(mini)):
+                mini[k].pop(j)
+            if (i + j) % 2 == 0:
+                cofactor[i][j] = determinant(mini)
+            if (i + j) % 2 == 1:
+                cofactor[i][j] = -1 * determinant(mini)
+    return cofactor
