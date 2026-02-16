b10119213b20e0c6b5074984a75303d5c3fd4124
MariamIssah <m.issah1@alustudent.com>
2026-02-16 19:38:58 +0000

    6-expectation.py

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -30,9 +30,11 @@ def expectation(X, pi, m, S):
         return None, None
     n, d = X.shape
     k = pi.shape[0]
+    if m.ndim != 2:
+        return None, None
     if m.shape[0] != k or m.shape[1] != d:
         return None, None
-    if S.shape[0] != k or S.shape[1] != d or S.shape[2] != d:
+    if S.ndim != 3 or S.shape[0] != k or S.shape[1] != d or S.shape[2] != d:
         return None, None
     weighted = np.zeros((k, n))
     for j in range(k):
